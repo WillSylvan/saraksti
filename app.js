cd8241715f39672ffc77c622956bc91e6eac2802
WillSylvan <silvis.silvestrs@gmail.com>
2016-12-08 21:50:37 +0200

    finishsaraksts

--- app.js
+++ app.js
@@ -7,15 +7,22 @@
 // }
 // console.log(number(11))
 
-let number = function (x)  {
-    let n = []
-     for (let i = x-1; i >= 0; i--) { 
-         n.push(i)
-    }
-    return n
-}
-console.log(number(10))
+// let number = function (x)  {
+//     let n = []
+//      for (let i = x-1; i >= 0; i--) { 
+//          n.push(i)
+//     }
+//     return n
+// }
+// console.log(number(10))
 
 
-
+ let number = function (x)  {
+     let n = []
+      for (let i = 0; i < x; i++) { 
+          n.push(i * 2)
+     }
+     return n
+ }
+ console.log(number(10))
 
