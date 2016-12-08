fffcea04d1afb2d5dd3c2f1aa5a0ddae9b6bdec2
WillSylvan <silvis.silvestrs@gmail.com>
2016-12-08 21:26:42 +0200

    2

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+// let number = function (x)  {
+//     let n = []
+//      for (let i = 0; i < x; i++) { 
+//          n.push(i)
+//     }
+//     return n
+// }
+// console.log(number(11))
+
 let number = function (x)  {
     let n = []
-     for (let i = 0; i < x; i++) { 
+     for (let i = x-1; i >= 0; i--) { 
          n.push(i)
     }
     return n
 }
-console.log(number(100))
+console.log(number(10))
 
 
 
